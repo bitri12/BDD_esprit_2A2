dfff0670fd376eb08635f5501ff868bebc4ae335
mohamedzrig17 <83378557+mohamedzrig17@users.noreply.github.com>
2021-12-12 19:49:00 +0100

    Add files via upload

---
+++ interface2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="interface2.css">
+    <title>interface anas</title>
+</head>
+<body>
+    <h1 >
+        Informations
+    </h1>
+    <label for="comment"></label>
+    <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+    <br><br><br><br><br>
+    <h3>
+       <button> Modifier</button>
+     
+       <button> Annuler</button>
+    </h3>
+</body>
+</html>
