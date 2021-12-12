97948529d17d62d1e25f6317930433079a5b6340
mohamedzrig17 <83378557+mohamedzrig17@users.noreply.github.com>
2021-12-12 19:49:46 +0100

    Add files via upload

---
+++ administration.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title>interface emna</title>
+  <link rel="stylesheet" href="Auto.css">
+</head>
+<body>
+    <h1>Administration</h1>
+   
+ <div class="menu">
+				 <a href="personnel.php"> Personnels</a>
+				 <a href="visiteur.php"> Visiteurs</a>
+                 <a href="Autorisation.php"> Autorisations </a>
+				  <a href="Histroique.php"> Historique</a> 
+                  </div>    
+			 
+ 
+ 
